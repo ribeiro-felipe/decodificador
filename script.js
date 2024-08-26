d9e8648f8e22c6924f25f25181bd6e5bcf933ce7
Felipe Delfino Ribeiro <49889245+ribeiro-felipe@users.noreply.github.com>
2024-08-26 18:27:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const textArea = document.querySelector (".text-box")
+const mensagem = document.querySelector (".message")
+
+// As "chaves" de criptografia que utilizaremos são:
+// A letra "e" é convertida para "enter"
+// A letra "i" é convertida para "imes"
+// A letra "a" é convertida para "ai"
+// A letra "o" é convertida para "ober"
+// A letra "u" é convertida para "ufat"
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value);
+    mensagem.value = textoEncriptado;
+    textArea.value = "";
+}
+
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e" , "enter"] , ["i" , "imes"] , ["a" , "ai"] , ["o" , "ober"] , ["u" , "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+
+    return stringEncriptada;
+}
+
+// Button 2 function
+
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensagem.value = textoDesencriptado;
+    textArea.value = "";
+}
+
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["e" , "enter"] , ["i" , "imes"] , ["a" , "ai"] , ["o" , "ober"] , ["u" , "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+
+    return stringDesencriptada;
+}
+
+
+// Copy button
+
+const input = document.querySelector(".message")
+
+function copyTo() {
+    navigator.clipboard.writeText(input.value).then(() => {
+        alert('Texto copiado!')
+    })
+}
